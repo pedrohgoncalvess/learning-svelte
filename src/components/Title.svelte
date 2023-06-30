1f277210edf4adc064b64cbfaf78a3e3ec15f86d
Pedro H <pedro_gonsalves@hotmail.com>
2023-06-30 00:49:13 -0300

    add: components

---
+++ src/components/Title.svelte
@@ -0,0 +1,17 @@
+<h1 class="title">Svelte profiles</h1>
+
+<style>
+    .title {
+        font-family: "Roboto";
+        font-style: normal;
+        font-weight: 800;
+        font-size: 38px;
+        line-height: 45px;
+
+        color: #3680f4;
+        background-image: linear-gradient(to left, #3680f4, #c14fac);
+        background-clip: text;
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+    }
+</style>
