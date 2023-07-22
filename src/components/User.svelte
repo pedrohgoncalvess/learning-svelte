b3d2496c8ba2adefd52767a7ec8580f4053a1125
Pedro H <pedro_gonsalves@hotmail.com>
2023-07-22 03:40:45 -0300

    add: recent repositories and conditions in svelte engine

--- src/components/User.svelte
+++ src/components/User.svelte
@@ -4,6 +4,8 @@ import type IUSers from "../interfaces/IUsers";
 
 export let user: IUSers;
 
+$: haveRepos = Boolean(user.recent_reposit.length)
+
 </script>
 <div class="card-user">
     <TopBar/>
@@ -35,6 +37,19 @@ export let user: IUSers;
                 Repositórios: <span>{user.public_repositorys}</span>
             </div>
         </div>
+
+        {#if haveRepos}
+        <div class="repositories">
+            <h2 class="title">Repositórios recentes</h2>
+            <ul>
+                {#each user.recent_reposit as reposit}
+                <li>
+                    <a class="repositorie" href="{reposit.url}" target="_blank" rel="noopener">{reposit.name}</a>
+                </li>
+                {/each}
+            </ul>
+        </div>
+        {/if}
     </div>
 </div>
 
@@ -84,5 +99,18 @@ export let user: IUSers;
         display: flex;
         justify-content: center;
         height: auto;
+    }
+
+    .repositories > .title {
+        font-size: 20px;
+        line-height: 31px;
+        font-weight: 600;
+        color: #395278;
+    }
+    .repositorie {
+        font-size: 20px;
+        line-height: 31px;
+        color: #6781a8;
+        transition: color 0.2s;
     }
 </style>
