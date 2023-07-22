dfa9e007deaf56f62e36e56808c7a91cc1a603e2
Pedro H <pedro_gonsalves@hotmail.com>
2023-07-22 03:39:17 -0300

    add: button component, working with slots

---
+++ src/components/Button.svelte
@@ -0,0 +1,38 @@
+<div class="button-container">
+    <button type="submit" class="button">
+        <slot></slot>
+    </button>
+</div>
+
+<style>
+
+    .button-container {
+        position: absolute;
+        width: 9.625rem;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        display: flex;
+    }
+
+    .button {
+        padding: 15px 24px;
+        border-radius: 8px;
+        border: none;
+        background: #2e80fa;
+        line-height: 26px;
+        color: #fff;
+        font-size: 22px;
+        cursor: pointer;
+
+        transition: background-color 0.2s;
+
+        display: flex;
+        align-items: center;
+        gap: 13px;
+    }
+
+    .button:hover {
+        background: #4590ff;
+    }
+</style>
